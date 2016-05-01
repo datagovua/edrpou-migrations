517120d771941885ebba34a6993bf5fcd23480e6
vanuan <vanuan@gmail.com>
2016-05-01 23:53:53 +0300

    Finish postgres

--- knexfile.js
+++ knexfile.js
@@ -3,11 +3,7 @@
 module.exports = {
 
   client: 'postgresql',
-  connection: {
-    host:     'postgres',
-    database: 'postgres',
-    user:     'postgres',
-  },
+  connection: process.env.PG_CONNECTION_STRING,
   pool: {
     min: 2,
     max: 10
